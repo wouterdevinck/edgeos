cd9ed8698b5ff0b3c612a9f13234a1ec959c6f5f
Wouter Devinck <wouter.devinck@gmail.com>
2022-10-27 22:29:08 +0200

    Add rebootp, add partitions

--- external/board/edgeos/post-build.sh
+++ external/board/edgeos/post-build.sh
@@ -14,3 +14,13 @@ fi
 install -D -m 0644 ${BR2_EXTERNAL_EDGEOS_PATH}/board/edgeos/autoboot.txt ${BINARIES_DIR}/autoboot.txt
 install -D -m 0644 ${BR2_EXTERNAL_EDGEOS_PATH}/board/edgeos/cmdline_a.txt ${BINARIES_DIR}/cmdline_a.txt
 install -D -m 0644 ${BR2_EXTERNAL_EDGEOS_PATH}/board/edgeos/cmdline_b.txt ${BINARIES_DIR}/cmdline_b.txt
+
+# Mount FAT partitions
+if [ -e ${TARGET_DIR}/etc/fstab ]; then
+	grep -qE '/dev/mmcblk0p1' ${TARGET_DIR}/etc/fstab || \
+	echo "/dev/mmcblk0p1 /autoboot vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
+	grep -qE '/dev/mmcblk0p2' ${TARGET_DIR}/etc/fstab || \
+	echo "/dev/mmcblk0p2 /boot_a vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
+	grep -qE '/dev/mmcblk0p3' ${TARGET_DIR}/etc/fstab || \
+	echo "/dev/mmcblk0p3 /boot_b vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
+fi
