1fe853bb958c22c17029d9f63db0b4b6f49449dc
Wouter Devinck <wouter.devinck@gmail.com>
2022-11-08 19:06:53 +0100

    Clean up

--- external/board/edgeos/rpi4-boot/post-build.sh
+++ external/board/edgeos/rpi4-boot/post-build.sh
@@ -7,3 +7,9 @@ set -e
 install -D -m 0644 ${BR2_EXTERNAL_EDGEOS_PATH}/board/edgeos/rpi4-boot/autoboot.txt ${BINARIES_DIR}/autoboot.txt
 install -D -m 0644 ${BR2_EXTERNAL_EDGEOS_PATH}/board/edgeos/rpi4-boot/config.txt ${BINARIES_DIR}/config.txt
 install -D -m 0644 ${BR2_EXTERNAL_EDGEOS_PATH}/board/edgeos/rpi4-boot/cmdline.txt ${BINARIES_DIR}/cmdline.txt
+
+# Reduce size of initramfs
+rm -rf ${TARGET_DIR}/lib/modules
+
+# Remove unused overlays
+find ${BINARIES_DIR}/rpi-firmware/overlays -type f -not -name 'miniuart-bt.dtbo' -delete
