f318d4cd4d4f86f756160c03148e54fe745e3beb
Wouter Devinck <wouter.devinck@gmail.com>
2022-11-08 23:22:54 +0100

    External toolchain for faster builds

--- Makefile
+++ Makefile
@@ -4,6 +4,10 @@ all: build
 prepare:
 	./build.sh prepare
 
+.PHONY: menuconfig-rpi4-toolchain
+menuconfig-rpi4-toolchain:
+	./build.sh menuconfig-rpi4-toolchain
+
 .PHONY: menuconfig-rpi4-boot
 menuconfig-rpi4-boot:
 	./build.sh menuconfig-rpi4-boot
@@ -11,6 +15,10 @@ menuconfig-rpi4-boot:
 .PHONY: menuconfig-rpi4-root
 menuconfig-rpi4-root:
 	./build.sh menuconfig-rpi4-root
+
+.PHONY: toolchain
+toolchain:
+	./build.sh toolchain
 
 .PHONY: build
 build:
--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -e
 
 BR_VERSION=2022.08.1
 
+CONFIG_RPI4_TOOLCHAIN=edgeos_rpi4_toolchain_defconfig
 CONFIG_RPI4_BOOT=edgeos_rpi4_boot_defconfig
 CONFIG_RPI4_ROOT=edgeos_rpi4_root_defconfig
 
@@ -12,11 +13,24 @@ EXTDIR="$SCRIPT_DIR/external"
 PATCHDIR="$SCRIPT_DIR/patches"
 OUTDIR="$SCRIPT_DIR/output"
 
+CONFPATH_RPI4_TOOLCHAIN="$EXTDIR/configs/$CONFIG_RPI4_TOOLCHAIN"
 CONFPATH_RPI4_BOOT="$EXTDIR/configs/$CONFIG_RPI4_BOOT"
 CONFPATH_RPI4_ROOT="$EXTDIR/configs/$CONFIG_RPI4_ROOT"
 
+OUTDIR_RPI4_TOOLCHAIN="$OUTDIR/rpi4-toolchain"
 OUTDIR_RPI4_BOOT="$OUTDIR/rpi4-boot"
 OUTDIR_RPI4_ROOT="$OUTDIR/rpi4-root"
+
+menuconfig () {
+  make O=$1 BR2_DEFCONFIG=$2 defconfig
+  make O=$1 BR2_EXTERNAL=$EXTDIR menuconfig
+  make O=$1 BR2_DEFCONFIG=$2 savedefconfig
+}
+
+build () {
+  make O=$1 BR2_EXTERNAL=$EXTDIR $2
+  make O=$1 $3
+}
 
 case $1 in
 
@@ -38,7 +52,7 @@ case $1 in
 
   ;;
 
-"build"|"menuconfig-rpi4-boot"|"menuconfig-rpi4-root")
+"build"|"toolchain"|"menuconfig-rpi4-toolchain"|"menuconfig-rpi4-boot"|"menuconfig-rpi4-root")
   if [ ! -d "$WORKDIR" ]; then
     printf "\nPlease run prepare first.\n\n"
     exit 2
@@ -46,27 +60,29 @@ case $1 in
   cd $WORKDIR
   ;;&
 
+"menuconfig-rpi4-toolchain")
+  menuconfig $OUTDIR_RPI4_TOOLCHAIN $CONFPATH_RPI4_TOOLCHAIN
+  ;;
+
 "menuconfig-rpi4-boot")
-  make O=$OUTDIR_RPI4_BOOT BR2_DEFCONFIG=$CONFPATH_RPI4_BOOT defconfig
-  make O=$OUTDIR_RPI4_BOOT BR2_EXTERNAL=$EXTDIR menuconfig
-  make O=$OUTDIR_RPI4_BOOT BR2_DEFCONFIG=$CONFPATH_RPI4_BOOT savedefconfig
+  menuconfig $OUTDIR_RPI4_BOOT $CONFPATH_RPI4_BOOT
   ;;
 
 "menuconfig-rpi4-root")
-  make O=$OUTDIR_RPI4_ROOT BR2_DEFCONFIG=$CONFPATH_RPI4_ROOT defconfig
-  make O=$OUTDIR_RPI4_ROOT BR2_EXTERNAL=$EXTDIR menuconfig
-  make O=$OUTDIR_RPI4_ROOT BR2_DEFCONFIG=$CONFPATH_RPI4_ROOT savedefconfig
+  menuconfig $OUTDIR_RPI4_ROOT $CONFPATH_RPI4_ROOT
+  ;;
+
+"toolchain")
+  build $OUTDIR_RPI4_TOOLCHAIN $CONFPATH_RPI4_TOOLCHAIN sdk
   ;;
 
 "build")
 
   # Build FAT boot partition - containing firmware (& config), kernel and initramfs
-  make O=$OUTDIR_RPI4_BOOT BR2_EXTERNAL=$EXTDIR $CONFIG_RPI4_BOOT
-  make O=$OUTDIR_RPI4_BOOT
+  build $OUTDIR_RPI4_BOOT $CONFIG_RPI4_BOOT
 
   # Build rootfs
-  make O=$OUTDIR_RPI4_ROOT BR2_EXTERNAL=$EXTDIR $CONFIG_RPI4_ROOT
-  make O=$OUTDIR_RPI4_ROOT
+  build $OUTDIR_RPI4_ROOT $CONFIG_RPI4_ROOT
 
   # Copy artifacts
   cp $OUTDIR_RPI4_BOOT/images/autoboot.vfat $OUTDIR
