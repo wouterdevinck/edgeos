8ffc3a977142305b7bf28d92bfa08314163d09e6
Wouter Devinck <wouter.devinck@gmail.com>
2022-12-20 14:16:41 +0100

    Add kernel modules to rootfs

--- external/board/edgeos/rpi4-root/post-build.sh
+++ external/board/edgeos/rpi4-root/post-build.sh
@@ -6,6 +6,11 @@ set -e
 # Version number
 install -D -m 0644 edgeos-version ${TARGET_DIR}/etc/edgeos-version
 
+# Add kernel modules
+BOOT_BINARIES_DIR=$(echo $BINARIES_DIR | sed 's/rpi4-root/rpi4-boot/g')
+mkdir -p ${TARGET_DIR}/lib/modules
+tar -xf ${BOOT_BINARIES_DIR}/modules.tar.gz -C ${TARGET_DIR}/lib/modules
+
 # Mount FAT partitions
 if [ -e ${TARGET_DIR}/etc/fstab ]; then
     mkdir -p ${TARGET_DIR}/boot/autoboot
