bec27e1541f7a99fd4b68ee2d8e2187279261ed5
Wouter Devinck <wouter.devinck@gmail.com>
2023-08-06 02:20:28 +0200

    Support booting from SSD instead of SD card

--- external/board/edgeos/rpi4-root/post-build.sh
+++ external/board/edgeos/rpi4-root/post-build.sh
@@ -10,13 +10,3 @@ install -D -m 0644 edgeos-version ${TARGET_DIR}/etc/edgeos-version
 BOOT_BINARIES_DIR=$(echo $BINARIES_DIR | sed 's/rpi4-root/rpi4-boot/g')
 mkdir -p ${TARGET_DIR}/lib/modules
 tar -xf ${BOOT_BINARIES_DIR}/modules.tar.gz -C ${TARGET_DIR}/lib/modules
-
-# Mount FAT partitions
-if [ -e ${TARGET_DIR}/etc/fstab ]; then
-    mkdir -p ${TARGET_DIR}/boot/autoboot
-    grep -qE '/dev/mmcblk0p1' ${TARGET_DIR}/etc/fstab || echo "/dev/mmcblk0p1 /boot/autoboot vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
-    mkdir -p ${TARGET_DIR}/boot/boot_a
-    grep -qE '/dev/mmcblk0p2' ${TARGET_DIR}/etc/fstab || echo "/dev/mmcblk0p2 /boot/boot_a vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
-    mkdir -p ${TARGET_DIR}/boot/boot_b
-    grep -qE '/dev/mmcblk0p3' ${TARGET_DIR}/etc/fstab || echo "/dev/mmcblk0p3 /boot/boot_b vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
-fi
