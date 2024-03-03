a75340553e4fa439377fdd5ce28bf31b94276e8c
Wouter Devinck <wouter.devinck@gmail.com>
2024-03-03 21:06:21 +0100

    Rudimentary online upgrade API endpoint

--- api/main.go
+++ api/main.go
@@ -7,16 +7,22 @@ import (
 	"os/exec"
 )
 
-type status struct {
+type statusResponse struct {
 	EdgeOsVersion      string `json:"edgeos_version"`
 	BootedFrom         string `json:"booted_from"`
 	ApplicationName    string `json:"application_name"`
 	ApplicationVersion string `json:"application_version"`
 }
 
+type upgradeRequest struct {
+	Url string `json:"url"`
+}
+
+// TODO Error handling
+
 func statusHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "Application/json")
-	var status status
+	var status statusResponse
 	osversion, _ := exec.Command("bash", "-c", "source current-boot > /dev/null; echo -n $CURRENT_EDGEOS_VERSION").Output()
 	status.EdgeOsVersion = string(osversion)
 	boot, _ := exec.Command("bash", "-c", "source current-boot > /dev/null; echo -n $CURRENT_BOOT").Output()
@@ -44,9 +50,15 @@ func switchBootHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// TODO Status reporting
 func upgradeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
-		// TODO Get url from body and run online-upgrade script
+		decoder := json.NewDecoder(r.Body)
+		var req upgradeRequest
+		err := decoder.Decode(&req)
+		if err == nil {
+			exec.Command("online-upgrade", req.Url).Run()
+		}
 	}
 }
 
